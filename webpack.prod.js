c9fb6083f4920562aee49febd95a14a9c3b0f049
maeligg <maelbrunet@gmail.com>
2021-07-09 14:38:28 +0200

    Split webpack dev and prod config

---
+++ webpack.prod.js
@@ -0,0 +1,51 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  devServer: {
+    contentBase: "./dist"
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "./dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'API_KEY': JSON.stringify(process.env.API_KEY),
+      }
+    })
+  ]
+};
